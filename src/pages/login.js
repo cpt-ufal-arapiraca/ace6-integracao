b4eb13348410a69edb12be1313a2f6680a4b2783
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2024-03-26 00:34:53 -0300

    feat: add whan logo to login screen

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,18 @@
 import Button from "../components/button"
 import InputField from "../components/inputfield"
 import sideImage from '../assets/images/login.png'
+import logo from '../assets/images/logo.png'
 
 function Login() {
     return (
         <div class="flex flex-row w-full">  
-        <div class="w-1/3">
-            <img class="object-cover h-full" src={sideImage}></img>
+        <div class="w-1/3 relative">
+            <img class="object-cover h-full absolute" src={sideImage}></img>
+            <div class="bg-gradient-to-b from-primary to-transparent absolute w-full h-full"></div>
+            <img class="w-2/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute" src={logo}></img>
+
+            
+
         </div>
         <div class="h-screen flex items-center justify-center w-2/3">
         <div class="flex flex-col w-1/3 gap-8">
