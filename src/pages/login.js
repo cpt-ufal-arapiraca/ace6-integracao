599ad632911bfd982ddb94c55ca6f657c79a88fe
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2024-03-26 00:17:52 -0300

    feat: organize login divs on screen

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,23 @@
+import Button from "../components/button"
 import InputField from "../components/inputfield"
+import sideImage from '../assets/images/login.png'
 
 function Login() {
     return (
-        <div>
+        <div class="flex flex-row w-full">  
+        <div class="w-1/3">
+            <img class="object-cover h-full" src={sideImage}></img>
+        </div>
+        <div class="h-screen flex items-center justify-center w-2/3">
+        <div class="flex flex-col w-1/3">
             <InputField title="Nome de Usuário" id="username"></InputField>
             <InputField title="Senha" id="password" type="password"></InputField>
+            <Button title="Entrar"></Button>
         </div>
+        </div>
+
+        </div>
+
     )
 }
 export default Login
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'login-background': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('/src/assets/images/login.png')"
+      },
       colors: {
         'primary': '#0095DA',
         'gray2': '#4F4F4F',
